0fb2d80d498422d200d35684e138e40533cd1016
Kelvin okumba <okumbakelvin@gmail.com>
2023-07-11 22:39:23 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+  let distance = (block - 42)
+  if (distance < 0) {
+      return (distance * -1)
+  } else{
+  return distance}
+}
+
+function distanceFromHqInFeet(block){
+  let distance = (block - 42) * 264
+  if (distance < 0) {
+      return (distance * -1) 
+  } else{
+  return distance
+  }
+}
+
+function distanceTravelledInFeet(start, destination) {
+  let distance = (destination - start) * 264
+  if (distance < 0) {
+      return (distance * -1)
+  } else {
+  return distance
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  let distanceTravelled = (destination - start) * 264
+  if (distanceTravelled <= 400) {
+      return 0 
+  } else if (distanceTravelled <= 2000) {
+      let fare = (distanceTravelled - 400) * 0.02;
+      return fare
+  } else if(distanceTravelled <= 2500) {
+      return 25
+  } else {
+      return "cannot travel that far"
+  }
+}
